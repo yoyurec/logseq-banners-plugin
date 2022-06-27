b15302b971edec9c233acb3c0ae0b5ffd74f92e6
sawhney17 <80150109+sawhney17@users.noreply.github.com>
2022-06-27 22:25:06 +0400

    remove utils and dev code

--- src/main.tsx
+++ src/main.tsx
@@ -98,41 +98,6 @@ function main() {
       }
     }, 100)
   }
-
-  function createModel() {
-    return {
-      show() {
-        // logseq.showMainUI();
-        // addImage()
-      },
-    };
-  }
-
-  logseq.provideModel(createModel());
-  logseq.setMainUIInlineStyle({
-    zIndex: 11,
-  });
-
-  const openIconName = "template-plugin-open";
-
-  logseq.provideStyle(css`
-    .${openIconName} {
-      opacity: 0.55;
-      font-size: 20px;
-      margin-top: 4px;
-    }
-
-    .${openIconName}:hover {
-      opacity: 0.9;
-    }
-  `);
-
-  logseq.App.registerUIItem("toolbar", {
-    key: openIconName,
-    template: `
-      <div data-on-click="show" class="${openIconName}">⚙️</div>
-    `,
-  });
 }
 
 logseq.ready(main).catch(console.error);
