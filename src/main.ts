925d40ed93bc5dc283cdb737021cb44ad36b31c2
yoyurec <yoyurec@gmail.com>
2022-07-27 22:45:58 +0300

    refactor(tsx): change tsx to ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,9 @@
 import "@logseq/libs";
 
 import { logseq as PL } from "../package.json";
-import { SettingSchemaDesc, PageEntity, BlockEntity } from "@logseq/libs/dist/LSPlugin.user";
+import { SettingSchemaDesc } from "@logseq/libs/dist/LSPlugin.user";
 
 import mainStyles from "./main.css?raw";
-
-const pluginId = PL.id;
-
-let root: HTMLElement;
-let body: HTMLElement;
 
 type AssetDataList = {
   [prop: string]: AssetData;
@@ -28,15 +23,85 @@ enum AssetType {
   pageIcon = "pageIcon"
 }
 
-let pageType: string;
+type WidgetsConfig = {
+  onlyOnJournals: boolean;
+  customHTML?: string;
+  calendar?: any;
+}
+
+const pluginId = PL.id;
+
+let doc: Document;
+let root: HTMLElement;
+let body: HTMLElement;
+
+let isJournal: boolean;
+let isHome: boolean;
+let isPage: boolean;
+
 let defaultConfig: AssetDataList;
 let customPropsConfig: AssetDataList;
+let widgetsConfig: WidgetsConfig;
 let timeout: number;
 let hidePluginProps: boolean;
 
 const settingsDefaultPageBanner = "https://wallpaperaccess.com/full/1146672.jpg";
 const settingsDefaultJournalBanner = "https://images.unsplash.com/photo-1646026371686-79950ceb6daa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1034&q=80";
+const settingsWidgetsCustomHTML = `
+<iframe id="banner-widgets-weather" src="https://indify.co/widgets/live/weather/7QOWaH4IPGGaAr4puql2"></iframe><iframe id="banner-widgets-pomo" src="https://pomofocus.io/app"></iframe>
+`;
+
+//<iframe id="banner-widgets-weather"  src="https://indify.co/widgets/live/weather/7QOWaH4IPGGaAr4puql2" style="width: 140px;height: 390px;position: absolute;top: -90px;left: 0;backdrop-filter: blur(6px);border-right: 1px solid #606060;"></iframe> <iframe id="banner-widgets-pomo" src="https://pomofocus.io/app" style="width: 182px;height: 110px;opacity: 0.6;filter: saturate(0.3);position: absolute;right: 0;top: 0;border-radius: 0 0 0 6px;"></iframe>
+
+//<iframe id="banner-widgets-weather"  src="https://indify.co/widgets/live/weather/7QOWaH4IPGGaAr4puql2" style="height: 110px;position: absolute;top: 0;left: 0;width: calc(100% - var(--widgetsCalendarWidth, 0));"></iframe><iframe id="banner-widgets-pomo" src="https://pomofocus.io/app" style="width: 150px;height: 110px;filter: saturate(0.3);position: absolute;top: 0;left: 0;border-radius: 0 10px 10px 0;"></iframe><style>#banner-widgets-calendar{left:unset;right:0}#banner-widgets-calendar .calendar-head, #banner-widgets-calendar .calendar-title height:55px;}</style>
+
 const settingsArray: SettingSchemaDesc[] = [
+  {
+    key: "generalHeading",
+    title: "General settings",
+    //@ts-expect-error
+    type: "heading"
+  },
+  {
+    key: "hidePluginProps",
+    title: "",
+    type: "boolean",
+    description: "Hide plugin-related page props? (will be shown only on edit)",
+    default: "false",
+  },
+  {
+    key: "widgetsHeading",
+    title: "Widgets common settings",
+    //@ts-expect-error
+    type: "heading"
+  },
+  {
+    key: "widgetsOnlyOnJournals",
+    title: "",
+    type: "boolean",
+    description: "Show widgets only on home and journals pages?",
+    default: true,
+  },
+  {
+    key: "widgetsCustomHTML",
+    title: "",
+    type: "string",
+    description: "Show custom HTML (iframe for ex.) as widget on home and journal pages",
+    default: settingsWidgetsCustomHTML,
+  },
+  {
+    key: "widgetsCalendarHeading",
+    title: "Widget calendar settings",
+    //@ts-expect-error
+    type: "heading"
+  },
+  {
+    key: "widgetsCalendarWidth",
+    title: "Block calendar widget width",
+    type: "string",
+    description: "",
+    default: "310px",
+  },
   {
     key: "journalHeading",
     title: "Journal and home settings",
@@ -120,17 +185,10 @@ const settingsArray: SettingSchemaDesc[] = [
     default: "40px",
   },
   {
-    key: "otherHeading",
-    title: "Other settings",
+    key: "adwancedHeading",
+    title: "Adwanced settings",
     //@ts-expect-error
     type: "heading"
-  },
-  {
-    key: "hidePluginProps",
-    title: "",
-    type: "boolean",
-    description: "Hide plugin-related page props? (will be shown only on edit)",
-    default: "false",
   },
   {
     key: "customPropsConfig",
@@ -168,16 +226,27 @@ const initStyles = () => {
   logseq.provideStyle(mainStyles);
 }
 
+const initGlobalCSSVars = () => {
+  initWidgetsCSSVars();
+}
+
 // Read settings
 const readPluginSettings = () => {
+  widgetsConfig = {
+    calendar: {},
+    onlyOnJournals: true
+  };
   defaultConfig = {
     page: {},
     journal: {}
   }
-  const pluginSettings = logseq.settings;
-  if (pluginSettings) {
+
+  if (logseq.settings) {
     ({
       hidePluginProps,
+      widgetsOnlyOnJournals: widgetsConfig.onlyOnJournals,
+      widgetsCustomHTML: widgetsConfig.customHTML,
+      widgetsCalendarWidth: widgetsConfig.calendar.width,
       defaultPageBanner: defaultConfig.page.banner,
       pageBannerHeight: defaultConfig.page.bannerHeight,
       pageBannerAlign: defaultConfig.page.bannerAlign,
@@ -190,7 +259,7 @@ const readPluginSettings = () => {
       journalIconWidth: defaultConfig.journal.iconWidth,
       customPropsConfig,
       timeout
-    } = pluginSettings);
+    } = logseq.settings);
   }
   encodeDefaultBanners();
 }
@@ -225,57 +294,67 @@ const encodeDefaultBanners = async () => {
   }
 }
 
-// Hide props
-const hideProps = () => {
-  const propBlockKeys = top?.document.getElementsByClassName("page-property-key");
+// Colors
+function hexToRGB(h: string): string {
+  let r = 0, g = 0, b = 0;
+  //@ts-expect-error
+  r = "0x" + h[1] + h[2];
+  //@ts-expect-error
+  g = "0x" + h[3] + h[4];
+  //@ts-expect-error
+  b = "0x" + h[5] + h[6];
+  return +r + "," + +g + "," + +b;
+}
+
+// Bg colors magic
+const setPrimaryColors = () => {
+  const primaryTextcolor = getComputedStyle(top!.document.documentElement).getPropertyValue('--ls-primary-text-color').trim();
+  root.style.setProperty("--widgetsTextColor", hexToRGB(primaryTextcolor));
+  const primaryBgcolor = getComputedStyle(top!.document.documentElement).getPropertyValue('--ls-primary-background-color').trim();
+  root.style.setProperty("--widgetsBgColor", hexToRGB(primaryBgcolor));
+
+}
+
+
+// Hide page props
+const hidePageProps = () => {
+  const propBlockKeys = doc.getElementsByClassName("page-property-key");
   if (propBlockKeys?.length) {
     for (let i = 0; i < propBlockKeys.length; i++) {
       if (propBlockKeys[i].textContent === "banner" || propBlockKeys[i].textContent === "page-icon" || propBlockKeys[i].textContent === "icon") {
-        propBlockKeys[i].parentElement!.style.display = hidePluginProps ? "none" : "block" ;
+        propBlockKeys[i].parentElement!.parentElement!.style.display = hidePluginProps ? "none" : "block" ;
       }
     }
   }
 }
 
-// Render
-const render = async () => {
-  hideProps();
-  pageType = getPageType();
-  if (!isPageTypeOk()) {
-    clearIcon();
-    clearBanner();
-    return;
-  }
-  const pageAssetsData: AssetData = await getPageAssetsData();
-  if (pageAssetsData) {
-    renderBanner(pageAssetsData);
-  }
-}
-
 // Get page type
 const getPageType = () => {
-  return body.getAttribute("data-page")!;
-}
-
-// Skip "system pasges", show/hide banner and icon
-const isPageTypeOk = () => {
-  if (pageType === "page" || pageType === "home") {
-    return true;
+  isPage = false;
+  isHome = false;
+  const pageType = body.getAttribute("data-page");
+  if (pageType === "home") {
+    isHome = true;
+    isPage = false;
   }
-  return false;
+  if (pageType === "page") {
+    isPage = true;
+    isHome = false;
+  }
 }
 
 const getPageAssetsData = async (): Promise<AssetData> => {
   let pageAssetsData = defaultConfig.journal;
   let currentPageProps: any = {};
   // home = journal page?
-  if (pageType === "home") {
+  if (isHome) {
     console.info(`#${pluginId}: Homepage`);
     return pageAssetsData;
   }
   // journal page?
   const currentPageData = await getPageData();
-  if (currentPageData["journal?"]) {
+  isJournal = currentPageData["journal?"];
+  if (isJournal) {
     console.info(`#${pluginId}: Journal page`);
     return pageAssetsData;
   }
@@ -329,6 +408,12 @@ const getCustomAssetData = (currentPageProps: any) => {
 // Render banner
 const renderBanner = async (pageAssetsData: AssetData) => {
   if (pageAssetsData.banner) {
+    // Set banner CSS variable
+    body.classList.add("is-banner-active");
+    root.style.setProperty("--bannerHeight", `${pageAssetsData.bannerHeight}`);
+    root.style.setProperty("--bannerAlign", `${pageAssetsData.bannerAlign}`);
+
+
     //remove surrounding quotations if present
     pageAssetsData.banner = pageAssetsData.banner.replace(/^"(.*)"$/, '$1');
     // if local image from assets folder
@@ -336,20 +421,17 @@ const renderBanner = async (pageAssetsData: AssetData) => {
       const graphPath = (await logseq.App.getCurrentGraph())?.path;
       pageAssetsData.banner = encodeURI("assets://" + graphPath + pageAssetsData.banner.replace("..", ""));
     }
-    // Set banner CSS variable
-    body.classList.add("is-banner-active");
     root.style.setProperty("--pageBanner", `url(${pageAssetsData.banner})`);
-    root.style.setProperty("--bannerHeight", `${pageAssetsData.bannerHeight}`);
-    root.style.setProperty("--bannerAlign", `${pageAssetsData.bannerAlign}`);
 
     // render icon only if banner exists
     renderIcon(pageAssetsData);
   } else {
+    // clear old banner
     clearBanner();
   }
 }
 
-// render icon
+// Render icon
 const renderIcon = async (pageAssetsData: AssetData) => {
   const pageIcon = pageAssetsData.icon || pageAssetsData.pageIcon;
   if (pageIcon) {
@@ -358,8 +440,24 @@ const renderIcon = async (pageAssetsData: AssetData) => {
     root.style.setProperty("--iconWidth", `${pageAssetsData.iconWidth}`);
     root.style.setProperty("--pageIcon", `"${pageIcon}"`);
   } else {
+    // clear old icon
     clearIcon();
   }
+}
+
+// Hide banner element
+const clearBanner = () => {
+  body.classList.remove("is-banner-active");
+  root.style.setProperty("--pageBanner", "");
+  root.style.setProperty("--bannerHeight", "");
+  root.style.setProperty("--bannerAlign", "");
+}
+
+// Hide icon element
+const clearIcon = () => {
+  body.classList.remove("is-icon-active");
+  root.style.setProperty("--pageIcon", "");
+  root.style.setProperty("--iconWidth", "");
 }
 
 // Page props was edited
@@ -389,28 +487,13 @@ const propsChangedObserverInit = () => {
   propsChangedObserver = new MutationObserver(propsChangedCallback);
 }
 const propsChangedObserverRun = () => {
-  const preBlock = top?.document.getElementsByClassName("content")[0]?.firstChild?.firstChild?.firstChild?.firstChild;
+  const preBlock = doc.getElementsByClassName("content")[0]?.firstChild?.firstChild?.firstChild?.firstChild;
   if (preBlock) {
     propsChangedObserver.observe(preBlock, propsChangedObserverConfig);
   }
 }
 const propsChangedObserverStop = () => {
   propsChangedObserver.disconnect();
-}
-
-// Hide banner element
-const clearBanner = () => {
-  body.classList.remove("is-banner-active");
-  root.style.setProperty("--pageBanner", "");
-  root.style.setProperty("--bannerHeight", "");
-  root.style.setProperty("--bannerAlign", "");
-}
-
-// Hide icon element
-const clearIcon = () => {
-  body.classList.remove("is-icon-active");
-  root.style.setProperty("--pageIcon", "");
-  root.style.setProperty("--iconWidth", "");
 }
 
 // Page changed
@@ -427,31 +510,137 @@ const routeChangedCallback = () => {
 
 // Setting changed
 const onSettingsChangedCallback = () => {
+  const oldWidgetsConfig = widgetsConfig;
   readPluginSettings();
+  initGlobalCSSVars();
   render();
- }
+  renderWidgetsCustom(oldWidgetsConfig);
+}
+
+// Plugin unloaded
+const onPluginUnloadCallback = () => {
+  // clean up
+  top!.document.getElementById("banner")?.remove();
+}
+
+// Color mode changed
+const onThemeModeChangedCallback = () => {
+  setTimeout(() => {
+    setPrimaryColors();
+  }, 100)
+}
+
+// Render placeholder
+const renderPlaceholder = () => {
+  // Widgets area
+  if (!doc.getElementById("banner")) {
+    const container = doc.getElementById("main-content-container");
+    if (container) {
+      container.setAttribute("style", "display:block;margin-left:auto;margin-right:auto;");
+      container.insertAdjacentHTML(
+        "afterbegin",
+        `<div id="banner" style="position:relative;display:none;">
+          <div id="banner-widgets">
+            <div id="banner-widgets-calendar"></div>
+            <div id="banner-widgets-custom"></div>
+          </div>
+
+        </div>`
+      );
+    }
+  }
+}
+
+// Show placeholder
+const showPlaceholder = () => {
+  doc.getElementById("banner")!.style.display = "block";
+}
+
+// Hide placeholder
+const hidePlaceholder = () => {
+  doc.getElementById("banner")!.style.display = "none";
+}
+
+// Show widgets placeholder
+const showWidgetsPlaceholder = () => {
+  doc.getElementById("banner-widgets")!.style.display = "block";
+}
+
+// Hide widgets placeholder
+const hideWidgetsPlaceholder = () => {
+  doc.getElementById("banner-widgets")!.style.display = "none";
+}
+
+// Render custom widgets HTML
+const renderWidgetsCustom = (oldWidgetsConfig?: WidgetsConfig) => {
+  const bannerWidgetsCustom = doc.getElementById("banner-widgets-custom");
+  if (bannerWidgetsCustom
+    && (!oldWidgetsConfig || (oldWidgetsConfig && (oldWidgetsConfig.customHTML !== widgetsConfig.customHTML)))) {
+      bannerWidgetsCustom.innerHTML = widgetsConfig.customHTML || "";
+  }
+}
+
+const initWidgetsCSSVars = () => {
+  setPrimaryColors();
+  root.style.setProperty("--widgetsCalendarWidth", widgetsConfig.calendar.width);
+}
+
+
+// Render
+const render = async () => {
+  getPageType();
+
+  if (!(isHome || isPage)) {
+    hidePlaceholder();
+    return;
+  }
+
+  hidePageProps();
+
+  const pageAssetsData: AssetData = await getPageAssetsData();
+  if (pageAssetsData) {
+    if (widgetsConfig.onlyOnJournals) {
+      if (!(isHome || isJournal)) {
+        hideWidgetsPlaceholder();
+      } else {
+        showWidgetsPlaceholder();
+      }
+    }
+    renderBanner(pageAssetsData);
+    showPlaceholder();
+  }
+}
 
 // On Logseq ready - MAIN
 const main = async () => {
   console.info(`#${pluginId}: MAIN`);
+
   logseq.useSettingsSchema(settingsArray);
-  root = top!.document.documentElement;
-  body = top!.document.body;
+  doc = top!.document;
+  root = doc.documentElement;
+  body = doc.body;
+
+  body.classList.add("is-banners-plugin-loaded");
 
   readPluginSettings();
   initStyles();
-    setTimeout(() => {
+  initGlobalCSSVars();
+  setTimeout(() => {
+    renderPlaceholder();
     render();
+    renderWidgetsCustom();
   }, timeout*2)
 
   // Listeners late run
   propsChangedObserverInit();
+
+  // Secondary listeners
   setTimeout(() => {
     // Listen for page props
     propsChangedObserverRun();
 
     // Listen for pages switch
-    logseq.App.onRouteChanged(async () => {
+    logseq.App.onRouteChanged( async () => {
       routeChangedCallback();
     })
 
@@ -459,6 +648,17 @@ const main = async () => {
     logseq.onSettingsChanged(() => {
       onSettingsChangedCallback();
     })
+
+    // Listen plugin unload
+    logseq.beforeunload( async () => {
+      onPluginUnloadCallback();
+    })
+
+    // Listen for theme mode changed
+    logseq.App.onThemeModeChanged( () => {
+      onThemeModeChangedCallback();
+    })
+
   }, 4000);
 
 }
